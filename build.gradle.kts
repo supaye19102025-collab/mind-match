03cc6c432b2aecd10d307fe7a624bc03a2a6424e
supaye19102025-collab <supaye19102025@gmail.com>
2026-07-07 21:50:42 +0630

    Fix gradle plugin version to 8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
 }
